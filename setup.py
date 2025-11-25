882a259555c884b84f00f5d55a0543132cc71a77
plugame <plugame.jp@gmail.com>
2025-11-25 23:52:18 +0900

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lora-utils",
-    version="0.1.0",
+    version="0.1.1",
     author="msqrd6",
     description="A utility library for LoRA (Low-Rank Adaptation) operations with PyTorch models",
     url="https://github.com/msqrd6/lora_utils",
